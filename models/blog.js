8144c4c25ea7381ceaad8979221993dd88c7d870
rebeccaloaustin <rebeccaloaustin@gmail.com>
2023-11-27 04:28:44 -0500

    some styling added

--- models/blog.js
+++ models/blog.js
@@ -4,6 +4,7 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   img: String,
+  date: String,
   createdAt: { type: Date, default: Date.now },
 });
 
